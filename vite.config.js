bbdeb767e5c4db78bb92f426cff8eb75e399435a
Ali Khonsari <alikhonsari@protonmail.com>
2026-07-05 14:07:48 -0400

    Enable enhanced report exports

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { scheduledStatusPlugin } from './scheduled-status-plugin.js'
 import { builderTagPlugin } from './builder-tag-plugin.js'
+import { reportExportPlugin } from './report-export-plugin.js'
 
 export default defineConfig({
-  plugins: [builderTagPlugin(), scheduledStatusPlugin(), react()],
+  plugins: [reportExportPlugin(), builderTagPlugin(), scheduledStatusPlugin(), react()],
   server: {
     host: '0.0.0.0',
     port: 5173,
