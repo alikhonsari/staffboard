5a0012ee7cac85730832f11fe84a81b2616d832c
Ali Khonsari <alikhonsari@protonmail.com>
2026-07-10 15:50:48 -0400

    Enable export context safeguards

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { enhancementSuitePlugin } from './enhancement-suite-plugin.js'
 import { shiftScopeCorePlugin } from './shift-scope-core-plugin.js'
 import { shiftOpsLogicPlugin } from './shift-ops-logic-plugin.js'
 import { shiftOpsSyntaxFixPlugin } from './shift-ops-syntax-fix-plugin.js'
+import { exportContextPlugin } from './export-context-plugin.js'
 import { enhancementNavPlugin } from './enhancement-nav-plugin.js'
 import { managerTabPlugin } from './manager-tab-plugin.js'
 import { auditTabPlugin } from './audit-tab-plugin.js'
@@ -27,7 +28,7 @@ import { pdfWeeklySectionPlugin } from './pdf-weekly-section-plugin.js'
 import { pdfFooterPlugin } from './pdf-footer-plugin.js'
 
 export default defineConfig({
-  plugins: [weekIsolationPlugin(), clearDayPlugin(), enhancementSuitePlugin(), shiftScopeCorePlugin(), shiftOpsLogicPlugin(), shiftOpsSyntaxFixPlugin(), enhancementNavPlugin(), managerTabPlugin(), auditTabPlugin(), toolsTabPlugin(), suggestionsTabPlugin(), shiftContextUxPlugin(), analysisScopePlugin(), auditAdminPlugin(), auditActionsPlugin(), shiftCorePlugin(), reportExportPlugin(), reportShiftFixPlugin(), pdfDailyDataPlugin(), pdfWeeklyDataPlugin(), pdfDailySectionPlugin(), pdfWeeklySectionPlugin(), pdfFooterPlugin(), builderTagPlugin(), scheduledStatusPlugin(), react()],
+  plugins: [weekIsolationPlugin(), clearDayPlugin(), enhancementSuitePlugin(), shiftScopeCorePlugin(), shiftOpsLogicPlugin(), shiftOpsSyntaxFixPlugin(), exportContextPlugin(), enhancementNavPlugin(), managerTabPlugin(), auditTabPlugin(), toolsTabPlugin(), suggestionsTabPlugin(), shiftContextUxPlugin(), analysisScopePlugin(), auditAdminPlugin(), auditActionsPlugin(), shiftCorePlugin(), reportExportPlugin(), reportShiftFixPlugin(), pdfDailyDataPlugin(), pdfWeeklyDataPlugin(), pdfDailySectionPlugin(), pdfWeeklySectionPlugin(), pdfFooterPlugin(), builderTagPlugin(), scheduledStatusPlugin(), react()],
   server: {
     host: '0.0.0.0',
     port: 5173,
