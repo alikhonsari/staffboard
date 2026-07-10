700b7a487cbf87ee7978fd9e9374cf044f3f92c6
Ali Khonsari <alikhonsari@protonmail.com>
2026-07-10 15:30:31 -0400

    Enable admin audit attribution

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import { managerTabPlugin } from './manager-tab-plugin.js'
 import { auditTabPlugin } from './audit-tab-plugin.js'
 import { toolsTabPlugin } from './tools-tab-plugin.js'
 import { suggestionsTabPlugin } from './suggestions-tab-plugin.js'
+import { auditAdminPlugin } from './audit-admin-plugin.js'
 import { shiftCorePlugin } from './shift-core-plugin.js'
 import { scheduledStatusPlugin } from './scheduled-status-plugin.js'
 import { builderTagPlugin } from './builder-tag-plugin.js'
@@ -20,7 +21,7 @@ import { pdfWeeklySectionPlugin } from './pdf-weekly-section-plugin.js'
 import { pdfFooterPlugin } from './pdf-footer-plugin.js'
 
 export default defineConfig({
-  plugins: [weekIsolationPlugin(), clearDayPlugin(), enhancementSuitePlugin(), enhancementNavPlugin(), managerTabPlugin(), auditTabPlugin(), toolsTabPlugin(), suggestionsTabPlugin(), shiftCorePlugin(), reportExportPlugin(), reportShiftFixPlugin(), pdfDailyDataPlugin(), pdfWeeklyDataPlugin(), pdfDailySectionPlugin(), pdfWeeklySectionPlugin(), pdfFooterPlugin(), builderTagPlugin(), scheduledStatusPlugin(), react()],
+  plugins: [weekIsolationPlugin(), clearDayPlugin(), enhancementSuitePlugin(), enhancementNavPlugin(), managerTabPlugin(), auditTabPlugin(), toolsTabPlugin(), suggestionsTabPlugin(), auditAdminPlugin(), shiftCorePlugin(), reportExportPlugin(), reportShiftFixPlugin(), pdfDailyDataPlugin(), pdfWeeklyDataPlugin(), pdfDailySectionPlugin(), pdfWeeklySectionPlugin(), pdfFooterPlugin(), builderTagPlugin(), scheduledStatusPlugin(), react()],
   server: {
     host: '0.0.0.0',
     port: 5173,
