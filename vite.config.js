5d40c01defd0f1c99da2cf654da824a20ab76418
Ali Khonsari <alikhonsari@protonmail.com>
2026-07-10 15:29:16 -0400

    Enable StaffBoard enhancement suite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { weekIsolationPlugin } from './week-isolation-plugin.js'
 import { clearDayPlugin } from './clear-day-plugin.js'
+import { enhancementSuitePlugin } from './enhancement-suite-plugin.js'
+import { enhancementNavPlugin } from './enhancement-nav-plugin.js'
+import { managerTabPlugin } from './manager-tab-plugin.js'
+import { auditTabPlugin } from './audit-tab-plugin.js'
+import { toolsTabPlugin } from './tools-tab-plugin.js'
+import { suggestionsTabPlugin } from './suggestions-tab-plugin.js'
 import { shiftCorePlugin } from './shift-core-plugin.js'
 import { scheduledStatusPlugin } from './scheduled-status-plugin.js'
 import { builderTagPlugin } from './builder-tag-plugin.js'
@@ -14,7 +20,7 @@ import { pdfWeeklySectionPlugin } from './pdf-weekly-section-plugin.js'
 import { pdfFooterPlugin } from './pdf-footer-plugin.js'
 
 export default defineConfig({
-  plugins: [weekIsolationPlugin(), clearDayPlugin(), shiftCorePlugin(), reportExportPlugin(), reportShiftFixPlugin(), pdfDailyDataPlugin(), pdfWeeklyDataPlugin(), pdfDailySectionPlugin(), pdfWeeklySectionPlugin(), pdfFooterPlugin(), builderTagPlugin(), scheduledStatusPlugin(), react()],
+  plugins: [weekIsolationPlugin(), clearDayPlugin(), enhancementSuitePlugin(), enhancementNavPlugin(), managerTabPlugin(), auditTabPlugin(), toolsTabPlugin(), suggestionsTabPlugin(), shiftCorePlugin(), reportExportPlugin(), reportShiftFixPlugin(), pdfDailyDataPlugin(), pdfWeeklyDataPlugin(), pdfDailySectionPlugin(), pdfWeeklySectionPlugin(), pdfFooterPlugin(), builderTagPlugin(), scheduledStatusPlugin(), react()],
   server: {
     host: '0.0.0.0',
     port: 5173,
